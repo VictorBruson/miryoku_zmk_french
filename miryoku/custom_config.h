5b1e27e20288b583f06af0a9aff3b99c6333a7e4
Victor B <145293037+VictorBruson@users.noreply.github.com>
2026-06-30 23:18:45 -0400

    commented gaming tap

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,8 +3,8 @@
 
 //gaming tap layer
 
-#define MIRYOKU_LAYER_TAP \
-&kp TAB,           &kp Q,             &kp W,             &kp E,             &kp 1,             &kp &to BASE,             &kp U,             &kp I,             &kp O,             &kp P,             \
-&kp LSHFT,         &kp A,             &kp S,             &kp D,             &kp 2,             &kp U_FUN,         &kp U_NUM,         &kp U_SYM,         &kp L,             &kp M,             \
-&kp LCTRL,         &kp 6,             &kp 5,             &kp 4,             &kp 3,             &kp U_MOUSE,       &kp U_NAV,         &kp U_MEDIA,       &kp SLASH,         &kp SQT,           \
-U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp TAB,           &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
+// #define MIRYOKU_LAYER_TAP \
+// &kp TAB,           &kp Q,             &kp W,             &kp E,             &kp 1,             &kp &to BASE,             &kp U,             &kp I,             &kp O,             &kp P,             \
+// &kp LSHFT,         &kp A,             &kp S,             &kp D,             &kp 2,             &kp U_FUN,         &kp U_NUM,         &kp U_SYM,         &kp L,             &kp M,             \
+// &kp LCTRL,         &kp 6,             &kp 5,             &kp 4,             &kp 3,             &kp U_MOUSE,       &kp U_NAV,         &kp U_MEDIA,       &kp SLASH,         &kp SQT,           \
+// U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp TAB,           &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
